43268dbf99b578305078a62ab3bdd629ad1c28db
dalal aljassem <dalal.aljassem@gmail.com>
2022-05-08 20:37:50 +0300

    shop done whats left image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './luke.jpeg';
 import './App.css';
+import products from './products.js';
 
+function Product(props){
+  return(
+    <div> 
+    <h1>{props.name}</h1>
+    <h2>{props.price}</h2>
+    <img src={props.image} alt="logo" />
+    </div>
+  )
+  
+}
+
+//rendering the array of components
 function App() {
+  const myProducts = products.map(item => <Product  name={item.name} price={item.price} image={item.image} /> )
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,9 +25,11 @@ function App() {
           This website will be a coffee and book shop run by Dee and Bee.
         </p>
         <img src={logo} alt="logo" />
+        {myProducts}
       </header>
     </div>
   );
+  
 }
 
 export default App;
---
+++ src/products.js
@@ -0,0 +1,10 @@
+var img = new Image();
+
+const products = [
+    {name: 'Americano', price: 23, image: img.src="americano.jpeg"}, 
+    {name: 'Latte', price: 15, image: img.src="latte.jpeg"}, 
+    {name: 'Matcha', price: 40, image: img.src="matcha.webp"}];
+
+export default products;
+
+//from a url 
