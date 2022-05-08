e11745274486fac42fa2381be52c91574dba2a03
dalal aljassem <dalal.aljassem@gmail.com>
2022-05-08 22:18:49 +0300

    shop done whats left image

--- src/products.js
+++ src/products.js
@@ -1,9 +1,9 @@
 var img = new Image();
 
 const products = [
-    {name: 'Americano', price: 23, image: img.src="americano.jpeg"}, 
-    {name: 'Latte', price: 15, image: img.src="latte.jpeg"}, 
-    {name: 'Matcha', price: 40, image: img.src="matcha.webp"}];
+    {name: 'Americano', price: 23, image: img.src="https://www.ostro.asia/wp-content/uploads/2020/11/03-02.png"}, 
+    {name: 'Latte', price: 15, image: img.src="https://www.monin.com/us//media/catalog/product/cache/15379ef9b25ecb17d7c23aeff27e4f55/f/1/f1763481-5179-4547-9a8f-0de9b07d6655-2.png"}, 
+    {name: 'Matcha', price: 40, image: img.src="http://cdn.shopify.com/s/files/1/1692/0875/products/matcha_latte_13cd32e9-e42c-4f1a-ad3e-1a1d1eb46993_grande.png?v=1612463670"}];
 
 export default products;
 
