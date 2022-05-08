283e346ff4b567a81b7fb5eb485710e2954a013c
dalal aljassem <dalal.aljassem@gmail.com>
2022-05-08 19:16:35 +0300

    my first react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './luke.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Dee and Bee's Shop</h3>
+        <p>
+          This website will be a coffee and book shop run by Dee and Bee.
+        </p>
+        <img src={logo} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
